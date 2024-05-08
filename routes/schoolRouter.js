2d4383d9975585e691d8784533c97e76842a881c
ashish <ashish140501@gmail.com>
2024-05-09 02:20:21 +0530

    multer

--- routes/schoolRouter.js
+++ routes/schoolRouter.js
@@ -7,8 +7,6 @@ const multer = require('multer');
 const { S3Client } = require('@aws-sdk/client-s3');
 const multerS3 = require('multer-s3');
 
-const uploadImages = multer({ dest: '../utils/upload' })
-
 //----controllers----//
 const { 
     enquiryCreateService,
